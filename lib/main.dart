486b502cf162352de38c688d6f30ebfbe8d044ed
Darin Ward <darinward23@gmail.com>
2026-03-21 19:37:45 -0400

    Add dashboard screen with habit list and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/add_habit_screen.dart';
+import 'screens/dashboard_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AddHabitScreen(),
+      home: const DashboardScreen(),
     );
   }
 }
